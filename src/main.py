aa3ca6c101913b89983209b6aa8a4a7b46e575d7
Supasin Liulak <palsp2406@gmail.com>
2024-09-04 22:05:26 +0700

    refactor

--- src/main.py
+++ src/main.py
@@ -31,27 +31,21 @@ def load_index(input_files, persist_dir = "./storage") -> VectorStoreIndex:
 
 
 
-index = load_index(input_files=["data/sa_hard_part.pdf"], persist_dir="./storage/sa")
-software_architecture_tool= QueryEngineTool.from_defaults(
-  index.as_query_engine(),
+
+sa_index = load_index(input_files=["data/sa_hard_part.pdf"], persist_dir="./storage/sa")
+sa_query_engine = sa_index.as_query_engine()
+sa_tool= QueryEngineTool.from_defaults(
+  sa_query_engine,
   name="software_architecture",
   description="A specialized query engine for 'Software Architecture: The Hard Parts' by Neal Ford et al. Provides detailed information on advanced software architecture concepts, trade-offs, patterns, and practices discussed in the book. Use for questions about architectural decisions, distributed systems design, and handling complexity in large-scale software systems."
 )
 
-
-index = load_index(input_files=["data/implementing_ddd.pdf"], persist_dir="./storage/ddd")
-ddd_tool= QueryEngineTool.from_defaults(
-  index.as_query_engine(),
-  name="domain_driven_design",
-  description="A specialized query engine for 'Implementing Domain-Driven Design' by Vaughn Vernon. Provides detailed information on domain-driven design concepts, patterns, and practices discussed in the book. Use for questions about domain modeling, bounded contexts, and event sourcing."
-)
-
 agent = ReActAgent.from_tools(
-    [software_architecture_tool, ddd_tool], verbose=True
+    [sa_tool], verbose=True
 )
 
 
 response = agent.chat(
-  "Can you rate Horror Story saga?"
+  "Can you describe horror story saga?"
 )
 print(response)
