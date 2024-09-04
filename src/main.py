580962003c04a798dfb617299e1980a74de3ceb0
Supasin Liulak <palsp2406@gmail.com>
2024-09-04 22:35:48 +0700

    reorganized tool

--- src/main.py
+++ src/main.py
@@ -13,14 +13,14 @@ import os
 # Add llm to global settings
 Settings.llm = OpenAI(model="gpt-3.5-turbo", temperature=0)
 
-def load_index(input_files, persist_dir = "./storage") -> VectorStoreIndex:
+def load_index(input_dir: str, persist_dir: str = "./storage") -> VectorStoreIndex:
   """
   Loads the index from the storage directory. If the storage directory does not exist, it will create the index and store it in the storage directory.
   """
   if not os.path.exists(persist_dir):
     print("Creating storage")
     # load the documents and create the index
-    documents = SimpleDirectoryReader(input_files=input_files).load_data()
+    documents = SimpleDirectoryReader(input_dir=input_dir).load_data()
     index = VectorStoreIndex.from_documents(documents)
     # store it for later
     index.storage_context.persist(persist_dir=persist_dir)
@@ -35,21 +35,30 @@ def load_index(input_files, persist_dir = "./storage") -> VectorStoreIndex:
 
 hyde = HyDEQueryTransform(include_original=True)
 
-sa_index = load_index(input_files=["data/sa_hard_part.pdf"], persist_dir="./storage/sa")
+sa_index = load_index(input_dir="data/software_architecture", persist_dir="./storage/software_architecture")
 sa_query_engine = sa_index.as_query_engine()
 sa_query_engine = TransformQueryEngine(sa_query_engine, hyde)
 sa_tool= QueryEngineTool.from_defaults(
   sa_query_engine,
   name="software_architecture",
-  description="A specialized query engine for 'Software Architecture: The Hard Parts' by Neal Ford et al. Provides detailed information on advanced software architecture concepts, trade-offs, patterns, and practices discussed in the book. Use for questions about architectural decisions, distributed systems design, and handling complexity in large-scale software systems."
+  description="A specialized query engine for Software Architecture. Provides detailed information on advanced software architecture concepts, trade-offs, patterns, and practices. Use for questions about architectural decisions, distributed systems design, and handling complexity in large-scale software systems."
+)
+
+db_index = load_index(input_dir="data/database", persist_dir="./storage/database")
+db_query_engine = db_index.as_query_engine()
+db_query_engine = TransformQueryEngine(db_query_engine, hyde)
+db_tool= QueryEngineTool.from_defaults(
+  db_query_engine,
+  name="database",
+  description="A specialized query engine for Database. Provides detailed information on advanced database concepts, trade-offs, patterns, and practices. Use for questions about database design and internal concepts."
 )
 
 agent = ReActAgent.from_tools(
-    [sa_tool], verbose=True
+    [sa_tool, db_tool], verbose=True
 )
 
 
 response = agent.chat(
-  "Can you rate horror story saga?"
+  "Why binary-tree is not used in database?"
 )
 print(response)
